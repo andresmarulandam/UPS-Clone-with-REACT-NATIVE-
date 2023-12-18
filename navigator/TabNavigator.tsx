36ecc1c715c4fdb4434f669c04f0431ee7375e17
Andres M <marulandaandres19@gmail.com>
2023-12-18 14:03:36 -0500

    Customers graphql firebase and stepzen

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CustomersScreen from '../screens/CustomersScreen';
 import OrdersScreen from '../screens/OrdersScreen';
+import { useNavigation } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
-const Tab = createBottomTabNavigator();
+export type TabStackParamList = {
+  Customers: undefined;
+  Orders: undefined;
+};
+
+const Tab = createBottomTabNavigator<TabStackParamList>();
 
 export default function TabNavigator() {
+  const navigate = useNavigation();
+
+  useLayoutEffect(() => {
+    navigate.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="customers" component={CustomersScreen} />
-      <Tab.Screen name="orders" component={OrdersScreen} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: 'red',
+        tabBarInactiveTintColor: 'blue',
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === 'Customers') {
+            return (
+              <FontAwesome
+                name="users"
+                size={24}
+                color={focused ? 'red' : 'blue'}
+              />
+            );
+          } else if (route.name === 'Orders') {
+            return (
+              <FontAwesome5
+                name="box-open"
+                size={24}
+                color={focused ? 'red' : 'blue'}
+              />
+            );
+          }
+        },
+      })}
+    >
+      <Tab.Screen name="Customers" component={CustomersScreen} />
+      <Tab.Screen name="Orders" component={OrdersScreen} />
     </Tab.Navigator>
   );
 }
--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,10 +1,59 @@
-import { View, Text, SafeAreaView } from 'react-native';
-import React from 'react';
+import { Image, StyleSheet, ScrollView, TextInput, View } from 'react-native';
+import React, { useLayoutEffect, useState } from 'react';
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from '@react-navigation/native';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { TabStackParamList } from '../navigator/TabNavigator';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack/lib/typescript/src/types';
+import { RootStackParamList } from '../navigator/RootNavigator';
+
+export type CustomersScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, 'Customers'>,
+  NativeStackNavigationProp<RootStackParamList>
+>;
 
 export default function CustomersScreen() {
+  const navigation = useNavigation<CustomersScreenNavigationProp>();
+  const [textInput, setTextInput] = useState<string>('');
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <SafeAreaView>
-      <Text>CustomersScreen</Text>
-    </SafeAreaView>
+    <ScrollView style={styles.scrollview}>
+      <Image
+        style={styles.image}
+        source={{ uri: 'https://links.papareact.com/3jc' }}
+      />
+
+      <TextInput
+        placeholder="Search by costumer"
+        value={textInput}
+        onChangeText={setTextInput}
+        style={styles.textInput}
+      />
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  scrollview: {
+    backgroundColor: '#59C1CC',
+  },
+  image: {
+    width: '100%',
+    height: 240,
+  },
+  textInput: {
+    backgroundColor: '#F2F3F4',
+    paddingTop: 5,
+    paddingBottom: 0,
+    paddingLeft: 30,
+    height: 60,
+  },
+});
